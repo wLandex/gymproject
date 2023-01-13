f766aad12eb343b9d770a964dc01fe56d43739b2
wLandexPC <landex617229@yandex.ru>
2023-01-13 19:10:26 +0300

    Add: RemovOne Remove Fix: Find

--- index.js
+++ index.js
@@ -10,12 +10,32 @@ class DB {
     this.data.push(obj);
     return obj["id"];
   }
-  find(filter) {
+  find(filter = {}) {
+    if (Object.keys(filter).length === 0) return this.data;
     return this.data.filter((record) => {
-      return Object.keys(filter).some((key) => {
+      return Object.keys(filter).every((key) => {
         return record[key] === filter[key];
       });
     });
+  }
+  findOne(filter) {
+    return this.find(filter)[0];
+  }
+
+  removeOne(filters) {
+    for (let [key, value] of Object.entries(filters)) {
+      for (let [i, arrVal] of this.data.entries()) {
+        if (arrVal[key] === value) return this.data.splice(i, 1);
+      }
+    }
+  }
+
+  remove(filters) {
+    for (let [key, value] of Object.entries(filters)) {
+      for (let [i, arrVal] of this.data.entries()) {
+        if (arrVal[key] === value) this.data.splice(i, 1);
+      }
+    }
   }
 }
 
@@ -39,16 +59,25 @@ class timetable {
   getTasks(filter) {
     return timeTableDb.find(filter);
   }
+
+  getTask(filter) {
+    return timeTableDb.findOne(filter);
+  }
+
   getTaskByID(id) {
-    return timeTableDb.find({ id });
+    return timeTableDb.findOne({ id });
+  }
+
+  removeTaskById(id) {
+    timeTableDb.removeOne({ id });
+  }
+  removeTasks(filter) {
+    timeTableDb.remove(filter);
   }
 }
 
 let ann = new timetable("Anya");
 
-const id1 = ann.addTask({ name: "Занятиe в зале", description: "3 по 15" });
+const id1 = ann.addTask({ name: "Занятие в зале", description: "3 по 15" });
 const id2 = ann.addTask({ name: "Занятие на улице", description: "Бег" });
-// console.log(ann.getTasks());
-console.log(id1, id2);
-
-console.log(ann.getTasks({ description: "3 по 15" }));
+const id3 = ann.addTask({ name: "Занятие в зале", description: "12 sdf" });
